a75b2574ef87c6bfa6583568cb2ba8563eef8b28
Omar Kayyali <okayyali2005@gmail.com>
2026-09-25 20:08:21 -0400

    task8: static code analyis; improving pylint score

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
+"""Flask server for emotion detection."""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
+
 @app.route('/emotionDetector')
 def emo_detector():
+    """Analyze the emotion of the submitted text."""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
     if response['dominant_emotion'] is None:
-        return f"Invalid text! Please try again!"
+        return "Invalid text! Please try again!"
 
     return f"""
         For the given statement, the system response
@@ -22,9 +26,9 @@ def emo_detector():
 
 @app.route('/')
 def render_index_page():
+    '''template loading function'''
     return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000)
-
